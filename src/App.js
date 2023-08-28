1d51961569b5507a68cf051299c8718c773e270e
mijinB <bmj13465@gmail.com>
2023-08-28 11:08:28 +0900

    styled components로 HTML 속성 변경(as사용)

--- src/App.js
+++ src/App.js
@@ -4,10 +4,18 @@ const Father = styled.div`
   display: flex;
 `;
 
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+
 function App() {
   return (
     <Father>
-      
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">Log in</Btn>
     </Father>
   );
 }
