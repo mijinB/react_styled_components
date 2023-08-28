a119674f513c6a16cd5ce86748b723db034b2844
mijinB <bmj13465@gmail.com>
2023-08-28 11:53:27 +0900

    styled components ThemeProvider 사용

--- src/App.js
+++ src/App.js
@@ -1,47 +1,22 @@
-import { styled, keyframes } from "styled-components";
+import { styled } from "styled-components";
+
+const Title = styled.h1`
+  color: ${props => props.theme.textColor};
+`;
 
 const Wrapper = styled.div`
   display: flex;
-`;
-
-const rotationAnimation = keyframes`
-  0% {
-    transform: rotate(0deg);
-    border-radius: 0px;
-  }
-  50% {
-    border-radius: 100px;
-  }
-  100% {
-    transform: rotate(360deg);
-    border-radius: 0px;
-  }
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: tomato;
-  display: flex;
+  width: 100vw;
+  height: 100vh;
   justify-content: center;
   align-items: center;
-  animation: ${rotationAnimation} 1s linear infinite;
-  ${Emoji}:hover {
-      font-size: 98px;
-    }
+  background-color: ${props => props.theme.backgroundColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😎</Emoji>
-      </Box>
-      <Emoji>😵</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
